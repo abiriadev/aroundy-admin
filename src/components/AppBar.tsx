082013d68747c9b9d7c41cf6eff2db7bd8705671
Abiria <abiria.dev@gmail.com>
2024-04-18 21:59:36 +0900

    build: apply tailwind

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,20 +1,15 @@
 import Box from '@mui/joy/Box'
 import List from '@mui/joy/List'
-import ListDivider from '@mui/joy/ListDivider'
 import ListItem from '@mui/joy/ListItem'
 import ListItemButton from '@mui/joy/ListItemButton'
 import Home from '@mui/icons-material/Home'
-import Person from '@mui/icons-material/Person'
 
 export const AppBar = () => {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
 			<List orientation="horizontal">
 				<ListItem>
-					<ListItemButton
-						component="a"
-						aria-label="Home"
-					>
+					<ListItemButton>
 						<Home />
 					</ListItemButton>
 				</ListItem>
