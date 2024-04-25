3c29d488b832d6f7483f29736d247d4c435ce5b2
Abiria <abiria.dev@gmail.com>
2024-04-25 20:39:38 +0900

    feat: add submit buttons

--- src/components/ArticleForm.tsx
+++ src/components/ArticleForm.tsx
@@ -3,6 +3,9 @@ import {
 	FormLabel,
 	Input,
 	Box,
+	ButtonGroup,
+	Button,
+	Stack,
 } from '@mui/joy'
 import { InputCaption } from './InputCaption'
 import { SelectCompanies } from './SelectCompanies'
@@ -14,88 +17,99 @@ import { SelectCategories } from './SelectCategories'
 export const ArticleForm = () => {
 	return (
 		<form>
-			<Box
-				display="grid"
-				gridTemplateColumns="auto 1fr"
-				gap={2}
-				// className="items-start"
-			>
-				<FormLabel>타이틀</FormLabel>
-				<FormControl>
-					<Input placeholder="아티클 제목" />
-				</FormControl>
+			<Stack gap={3}>
+				<Box
+					display="grid"
+					gridTemplateColumns="auto 1fr"
+					gap={2}
+					// className="items-start"
+				>
+					<FormLabel>타이틀</FormLabel>
+					<FormControl>
+						<Input placeholder="아티클 제목" />
+					</FormControl>
 
-				<FormLabel>카테고리</FormLabel>
-				<FormControl>
-					<SelectCategories />
-				</FormControl>
+					<FormLabel>카테고리</FormLabel>
+					<FormControl>
+						<SelectCategories />
+					</FormControl>
 
-				<FormLabel>기업</FormLabel>
-				<FormControl>
-					<SelectCompanies />
-				</FormControl>
+					<FormLabel>기업</FormLabel>
+					<FormControl>
+						<SelectCompanies />
+					</FormControl>
 
-				<FormLabel>피드</FormLabel>
-				<FormControl>
-					<Input
-						placeholder="사진"
-						type="file"
-						slotProps={{
-							input: {
-								multiple: true,
-							},
-						}}
-					/>
-				</FormControl>
+					<FormLabel>피드</FormLabel>
+					<FormControl>
+						<Input
+							placeholder="사진"
+							type="file"
+							slotProps={{
+								input: {
+									multiple: true,
+								},
+							}}
+						/>
+					</FormControl>
 
-				<FormLabel>캡션</FormLabel>
-				<FormControl>
-					<InputCaption />
-				</FormControl>
+					<FormLabel>캡션</FormLabel>
+					<FormControl>
+						<InputCaption />
+					</FormControl>
 
-				<FormLabel>이벤트 유형</FormLabel>
-				<FormControl>
-					<EventChannel />
-				</FormControl>
+					<FormLabel>이벤트 유형</FormLabel>
+					<FormControl>
+						<EventChannel />
+					</FormControl>
 
-				<FormLabel>주소</FormLabel>
-				<FormControl>
-					<Input placeholder="서울특별시 강남구 테헤란로44길" />
-				</FormControl>
+					<FormLabel>주소</FormLabel>
+					<FormControl>
+						<Input placeholder="서울특별시 강남구 테헤란로44길" />
+					</FormControl>
 
-				<FormLabel>연락처</FormLabel>
-				<FormControl>
-					<Input
-						placeholder="010-1234-5678"
-						type="tel"
-					/>
-				</FormControl>
+					<FormLabel>연락처</FormLabel>
+					<FormControl>
+						<Input
+							placeholder="010-1234-5678"
+							type="tel"
+						/>
+					</FormControl>
 
-				<FormLabel>게시일</FormLabel>
-				<FormControl>
-					<DateTimePicker />
-				</FormControl>
+					<FormLabel>게시일</FormLabel>
+					<FormControl>
+						<DateTimePicker />
+					</FormControl>
 
-				<FormLabel>이벤트 시작일</FormLabel>
-				<FormControl>
-					<DateTimePicker />
-				</FormControl>
+					<FormLabel>이벤트 시작일</FormLabel>
+					<FormControl>
+						<DateTimePicker />
+					</FormControl>
 
-				<FormLabel>이벤트 종료일</FormLabel>
-				<FormControl>
-					<DateTimePicker />
-				</FormControl>
+					<FormLabel>이벤트 종료일</FormLabel>
+					<FormControl>
+						<DateTimePicker />
+					</FormControl>
 
-				<FormLabel>출처 링크</FormLabel>
-				<FormControl>
-					<Input placeholder="https://example.com" />
-				</FormControl>
+					<FormLabel>출처 링크</FormLabel>
+					<FormControl>
+						<Input placeholder="https://example.com" />
+					</FormControl>
 
-				<FormLabel>태그</FormLabel>
-				<FormControl>
-					<SelectTags />
-				</FormControl>
-			</Box>
+					<FormLabel>태그</FormLabel>
+					<FormControl>
+						<SelectTags />
+					</FormControl>
+				</Box>
+				<Stack direction="row" gap={2}>
+					<Button
+						variant="outlined"
+						className="flex-1"
+					>
+						취소
+					</Button>
+					<Button className="flex-1">등록</Button>
+				</Stack>
+			</Stack>
 		</form>
 	)
 }
