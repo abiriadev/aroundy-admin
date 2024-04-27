3516e425c7c59a67acdaa2461023b12df6d78fba
Abiria <abiria.dev@gmail.com>
2024-04-27 09:58:18 +0900

    feat: use green theme

--- src/theme.ts
+++ src/theme.ts
@@ -2,23 +2,23 @@ import { koKR } from '@mui/x-date-pickers/locales'
 import { experimental_extendTheme as materialExtendTheme } from '@mui/material/styles'
 import { extendTheme as extendJoyTheme } from '@mui/joy/styles'
 
-const black = '#000000'
+const primary = '#12B890'
 
 export const joyTheme = extendJoyTheme({
 	colorSchemes: {
 		light: {
 			palette: {
 				primary: {
-					'50': '#fafafa',
-					'100': '#f5f5f5',
-					'200': '#e5e5e5',
-					'300': '#d4d4d',
-					'400': '#a3a3a3',
-					'500': black,
-					'600': '#525252',
-					'700': '#404040',
-					'800': '#262626',
-					'900': '#0a0a0a',
+					'50': '#ecfdf7',
+					'100': '#d1fae9',
+					'200': '#a8f2d7',
+					'300': '#6fe6c2',
+					'400': '#36d1a7',
+					'500': '#12b890',
+					'600': '#079475',
+					'700': '#057760',
+					'800': '#075e4e',
+					'900': '#074d41',
 				},
 			},
 		},
@@ -31,7 +31,7 @@ export const muiTheme = materialExtendTheme(
 			light: {
 				palette: {
 					primary: {
-						main: black,
+						main: primary,
 					},
 				},
 			},
