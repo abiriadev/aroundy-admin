2a5a94f440f9340dd52d74c928705897dd8f4eb2
Abiria <abiria.dev@gmail.com>
2024-05-20 19:53:32 +0900

    feat: add a new company page component

---
+++ src/pages/NewCompanyPage.tsx
@@ -0,0 +1,15 @@
+import { Grid } from '@mui/joy'
+import { CompanyForm } from '../components/CompanyForm'
+
+export const NewArticlePage = () => {
+	return (
+		<Grid container spacing={3}>
+			<Grid xs></Grid>
+			<Grid xs={6}>
+				<h2>새 기업 등록</h2>
+				<CompanyForm />
+			</Grid>
+			<Grid xs></Grid>
+		</Grid>
+	)
+}
