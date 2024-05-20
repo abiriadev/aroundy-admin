a47696559e793d883e165111cd715fedf6bdb9ad
Abiria <abiria.dev@gmail.com>
2024-05-20 19:35:56 +0900

    feat: add a title to new post page

--- src/pages/NewArticlePage.tsx
+++ src/pages/NewArticlePage.tsx
@@ -6,6 +6,7 @@ export const NewArticlePage = () => {
 		<Grid container spacing={3}>
 			<Grid xs></Grid>
 			<Grid xs={6}>
+				<h2>새 포스트 생성</h2>
 				<ArticleForm />
 			</Grid>
 			<Grid xs></Grid>
