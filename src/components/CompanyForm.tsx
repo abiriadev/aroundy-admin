79ac18ea59f01d4496a371cbf88a392eac09f6a0
Abiria <abiria.dev@gmail.com>
2024-05-20 19:05:45 +0900

    refactor: apply RHF

--- src/components/CompanyForm.tsx
+++ src/components/CompanyForm.tsx
@@ -8,33 +8,50 @@ import {
 	RadioGroup,
 	Radio,
 } from '@mui/joy'
+import { UploadImage } from './UploadImage'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+interface CompanyCreate {
+	name: string
+	status: 'active' | 'inactive'
+}
 
 export const CompanyForm = () => {
+	const { register, handleSubmit } =
+		useForm<CompanyCreate>()
+	const navigate = useNavigate()
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit(() => navigate('/'))}>
 			<Stack gap={3}>
-				<Box gap={2}>
-					<FormControl>
-						<FormLabel>기업명</FormLabel>
-						<Input placeholder="ex) 주식회사 세종대왕" />
-					</FormControl>
-					<FormControl>
-						<FormLabel>상태</FormLabel>
-						<RadioGroup
-							defaultValue="active"
-							orientation="horizontal"
-						>
-							<Radio
-								value="active"
-								label="활성화"
-							/>
-							<Radio
-								value="inactive"
-								label="비활성화"
-							/>
-						</RadioGroup>
-					</FormControl>
-				</Box>
+				<FormControl>
+					<FormLabel>기업명</FormLabel>
+					<Input
+						placeholder="ex) 주식회사 세종대왕"
+						{...register('name')}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormLabel>기업 로고</FormLabel>
+					<UploadImage />
+				</FormControl>
+				<FormControl>
+					<FormLabel>상태</FormLabel>
+					<RadioGroup
+						defaultValue="active"
+						orientation="horizontal"
+					>
+						<Radio
+							value="active"
+							label="활성화"
+						/>
+						<Radio
+							value="inactive"
+							label="비활성화"
+						/>
+					</RadioGroup>
+				</FormControl>
 				<Stack direction="row" gap={2}>
 					<Button
 						variant="outlined"
