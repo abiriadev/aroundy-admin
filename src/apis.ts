3d020ffca0056fa7697507e1228c03285f4ed6cd
Abiria <abiria.dev@gmail.com>
2024-06-03 20:51:38 +0900

    refactor: separate data and apis

--- src/apis.ts
+++ src/apis.ts
@@ -1,5 +1,32 @@
 import { companies } from './mocks/data.ts'
 
+export interface ArticleData {
+	id: string
+	title: string
+	category: string
+	feeds: Array<string>
+	caption: string
+	type: string
+	location: string
+	contact: string
+	createdAt: Date
+	publishAt: Date
+	startsAt: Date
+	endsAt: Date
+	link: string
+	company: {
+		id: string
+		name: string
+		logo: string
+	}
+	tags: Array<{
+		id: string
+		displayName: string
+	}>
+	likes: number
+	views: number
+}
+
 export interface Company {
 	id: string
 	name: string
