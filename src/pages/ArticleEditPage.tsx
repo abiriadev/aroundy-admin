64b50e3f3f5e61faf64dbeea8e867f4f4a9d64c9
Abiria <abiria.dev@gmail.com>
2024-06-03 20:54:28 +0900

    feat: show props to edit

--- src/pages/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage.tsx
@@ -1,13 +1,20 @@
 import { Grid } from '@mui/joy'
 import { ArticleForm } from '../components/ArticleForm'
+import { useParams } from 'react-router-dom'
+import { articles } from '../mocks/data'
 
 export const ArticleEditPage = () => {
+	const { id } = useParams()
+
 	return (
 		<Grid container spacing={3}>
 			<Grid xs></Grid>
 			<Grid xs={6}>
 				<h2>포스트 수정</h2>
-				<ArticleForm />
+				<ArticleForm
+					{...(articles.find(_ => _.id === id) ??
+						{})}
+				/>
 			</Grid>
 			<Grid xs></Grid>
 		</Grid>
